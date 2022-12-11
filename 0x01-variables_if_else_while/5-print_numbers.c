bb11e6e911ba63a6e0fd15a90c5faf0a033b9f42
Joshua <akalihujoshua@gmail.com>
2022-12-11 14:16:53 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ *main - single numbers of base 10 starting from 0
+ *followed by a new line
+ *return : 0 (Success)
+ */
+
+int main(0)
+{
+int a;
+a = 0;
+while (a < 10)
+printf("%d/n", a);
+a++;
+return (0);
+}
